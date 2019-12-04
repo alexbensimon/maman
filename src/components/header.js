660349ddc9a37cf62b874835afcac2fd4d69fbdb
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2019-12-04 19:27:37 +0100

    Add Contact page

--- src/components/header.js
+++ src/components/header.js
@@ -21,6 +21,9 @@ export const Header = () => {
         <Link to="/qigong/">
           <h3>Qi Gong</h3>
         </Link>
+        <Link to="/contact/">
+          <h3>Contact</h3>
+        </Link>
       </Menu>
     </Container>
   );
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Layout } from '../components/layout';
+import { Header } from '../components/header';
+
+export default () => {
+  return (
+    <Layout>
+      <Header />
+      <h1>Contact</h1>
+    </Layout>
+  );
+};
