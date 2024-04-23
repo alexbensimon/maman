6a89075a6db70e74ffe2f1c91177a4c6c90ff0a8
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2024-04-23 15:14:16 +0200

    Change info text

--- src/pages/infos.js
+++ src/pages/infos.js
@@ -47,8 +47,8 @@ export default ({ location, data }) => (
             .
           </LinkContainer>
           <List>
-            <li>Durée de la séance : 1 heure</li>
-            <li>Prix : 80 €</li>
+            <li>1 séance : 75 €</li>
+            <li>3 séances : 195 €</li>
           </List>
           <BaseLink href="https://g.co/kgs/VvJW5q">
             <Flex>
